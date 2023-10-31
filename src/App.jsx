3be17c68ed26ffa2826edf5f5c2248335da0dd10
easakibahmad <sakib.sgc.du@gmail.com>
2023-10-31 18:31:33 +0600

    initial setup done and home route created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello sakib</h1>
+      <button class="btn btn-secondary">Two</button>
+    </>
+  );
+}
+
+export default App
